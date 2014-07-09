57da41f867f7e29d7b6fcc0344e789063375a787
Janine Alexis <JanineGallaron@gmail.com>
2014-07-09 11:50:42 +0800

    Tabs Manager Added

---
+++ gzTrackz/src/com/example/tabs/HomeFragment.java
@@ -0,0 +1,21 @@
+package com.example.tabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gztrackz.R;
+
+public class HomeFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+		
+		return rootView;
+	}
+}
