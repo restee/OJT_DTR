ab57680fc2e267167619da23a89e250a8debf1a0
Warren <warrencaruana1@gmail.com>
2014-07-23 11:25:19 +0800

    Added files in res folder

--- gzTrackz/gen/com/example/gztrackz/R.java
+++ gzTrackz/gen/com/example/gztrackz/R.java
@@ -47,51 +47,57 @@ public final class R {
         public static final int powered=0x7f020011;
         public static final int rectangle=0x7f020012;
         public static final int roundbuttonstyle=0x7f020013;
-        public static final int table_border=0x7f020014;
+        public static final int standups_listview_shape=0x7f020014;
+        public static final int table_border=0x7f020015;
+        public static final int table_border_header=0x7f020016;
     }
     public static final class id {
         public static final int TextView01=0x7f090011;
         public static final int TextView03=0x7f090016;
-        public static final int action_logout=0x7f090029;
-        public static final int action_settings=0x7f090028;
+        public static final int action_logout=0x7f09002d;
+        public static final int action_settings=0x7f09002c;
         public static final int ampmTxt=0x7f09001a;
         public static final int confirmpasswordEditText=0x7f090008;
         public static final int dateTextView=0x7f09001f;
         public static final int dateTxt=0x7f09001b;
-        public static final int daySpinner=0x7f090026;
+        public static final int daySpinner=0x7f090025;
         public static final int emailRegisterEditText=0x7f090006;
         public static final int firstnameEditText=0x7f09000d;
         public static final int firstnameTextView=0x7f09000c;
-        public static final int gpsimage=0x7f090023;
-        public static final int historydateitem=0x7f090020;
+        public static final int gpsimage=0x7f090024;
+        public static final int historydateitem=0x7f090021;
         public static final int historyquerybutton=0x7f09001e;
-        public static final int historytimeinitem=0x7f090021;
-        public static final int historytimeoutitem=0x7f090022;
+        public static final int historytimeinitem=0x7f090022;
+        public static final int historytimeoutitem=0x7f090023;
         public static final int imageView1=0x7f090003;
         public static final int imageView2=0x7f090005;
         public static final int lastnameEditText=0x7f09000f;
         public static final int lastnameTextView=0x7f09000e;
         public static final int logInButton=0x7f090004;
-        public static final int monthSpinner=0x7f090025;
+        public static final int monthSpinner=0x7f090028;
         public static final int name=0x7f090018;
         public static final int pager=0x7f090017;
         public static final int passwordEditText=0x7f090001;
         public static final int passwordRegisterEditText=0x7f090009;
-        public static final int querybutton=0x7f090027;
+        public static final int querybutton=0x7f090026;
         public static final int registerButton=0x7f090010;
         public static final int registerTextView=0x7f090000;
         public static final int standupdone=0x7f090013;
+        public static final int standupsdate=0x7f090029;
+        public static final int standupslist=0x7f09001d;
         public static final int standupsnow=0x7f090014;
+        public static final int standupspreviewmessage=0x7f09002b;
         public static final int standupsprevious=0x7f090012;
         public static final int standupsproblem=0x7f090015;
+        public static final int standupstime=0x7f09002a;
         public static final int textView1=0x7f09000b;
         public static final int textView2=0x7f090007;
         public static final int textView3=0x7f09000a;
         public static final int timeLogBTN=0x7f09001c;
         public static final int timeTxt=0x7f090019;
-        public static final int timeintimeoutlist=0x7f09001d;
+        public static final int timeintimeoutlist=0x7f090020;
         public static final int usernameEditText=0x7f090002;
-        public static final int yearSpinner=0x7f090024;
+        public static final int yearSpinner=0x7f090027;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -103,6 +109,7 @@ public final class R {
         public static final int fragment_timestamp=0x7f030006;
         public static final int history_timein_timeout_item=0x7f030007;
         public static final int query_history_dialog=0x7f030008;
+        public static final int standups_list_item=0x7f030009;
     }
     public static final class menu {
         public static final int main=0x7f080000;
