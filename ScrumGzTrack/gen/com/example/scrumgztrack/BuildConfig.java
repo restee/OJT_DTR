ea67288e3a8827858ac5d5c8b93c546341f7a8bf
Janine Alexis <JanineGallaron@gmail.com>
2014-08-01 11:04:40 +0800

    Added new app

---
+++ ScrumGzTrack/gen/com/example/scrumgztrack/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.scrumgztrack;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ScrumGzTrack/src/com/example/tabs/TeamListFragment.java
@@ -0,0 +1,22 @@
+package com.example.tabs;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.scrumgztrack.R;
+
+public class TeamListFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_teamlist, container, false);
+		
+		return rootView;
+	}
+}
