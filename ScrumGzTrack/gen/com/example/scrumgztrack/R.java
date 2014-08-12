8bbb10bba00496037fd090185842454278449936
Janine Alexis <JanineGallaron@gmail.com>
2014-08-12 14:06:16 +0800

    Polished UI

--- ScrumGzTrack/gen/com/example/scrumgztrack/R.java
+++ ScrumGzTrack/gen/com/example/scrumgztrack/R.java
@@ -25,10 +25,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int button=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int ic_navigation_next_item=0x7f020002;
-        public static final int ic_plus=0x7f020003;
-        public static final int ic_social_add_group=0x7f020004;
+        public static final int ic_icon=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_navigation_next_item=0x7f020003;
+        public static final int ic_plus=0x7f020004;
+        public static final int ic_social_add_group=0x7f020005;
     }
     public static final class id {
         public static final int action_settings=0x7f09000b;
